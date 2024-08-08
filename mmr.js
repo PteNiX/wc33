ac8dbc420a6b119d1298325e7e23d8ef65d80eca
Aliaksandr Ptsitsa <ptenix@gmail.com>
2024-08-09 00:04:06 +0300

    feat: add 19 season

--- mmr.js
+++ mmr.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
         );
 });
 
-document.querySelector(".select").value = 18;
+document.querySelector(".select").value = 19;
 let s = document.querySelector(".select").value;
 
 let pten = "PteN#21803";
